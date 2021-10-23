62afd4d357d34d5b0ec4c3646920bd9134eeb563
Victor Lava <github@victorlava.com>
2021-10-24 00:52:42 +0300

    call bodyPartAseeder, userSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             PaymentSeeder::class,
+            BodyPartSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
