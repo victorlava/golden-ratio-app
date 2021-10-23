ae4bc68c09eb682075055f610af985cc399cdb52
Victor Lava <github@victorlava.com>
2021-10-23 22:22:22 +0300

    added payment model and factory

---
+++ app/Models/Payment.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payment extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'slug',
+        'is_active',
+        'is_healthy',
+    ];
+
+}
